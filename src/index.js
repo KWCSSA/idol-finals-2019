f3497f5322f88f717a50142a514f409278f50161
BruceWang1030 <r395wang@uwaterloo.ca>
2019-11-15 03:16:49 -0500

    init admin server

--- src/index.js
+++ src/index.js
@@ -1,39 +1,71 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const { sysLogger } = require('./utils/Logger');
+const { sysLogger } = require("./utils/Logger");
 
 var app = express();
 
-if (process.env.NODE_ENV === 'production') {
-	mongoose.connect(
-		`mongodb://${process.env.ADMIN_USERNAME}:${process.env
-			.ADMIN_DBPASS}@172.105.28.135:27017/idol-finals-2019?authSource=admin`,
-		{
-			useNewUrlParser: true,
-			useUnifiedTopology: true
-		}
-	);
+if (process.env.NODE_ENV === "production") {
+  mongoose.connect(
+    `mongodb://${process.env.ADMIN_USERNAME}:${process.env.ADMIN_DBPASS}@172.105.28.135:27017/idol-finals-2019?authSource=admin`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  );
 } else {
-	mongoose.connect('mongodb://localhost/idol-finals-2019', { useNewUrlParser: true, useUnifiedTopology: true });
+  mongoose.connect("mongodb://localhost/idol-finals-2019", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 }
 var db = mongoose.connection;
-db.on('error', error => {
-	sysLogger.log('error', 'Database connection error');
+db.on("error", error => {
+  sysLogger.log("error", "Database connection error");
 });
-db.once('open', () => {
-	runApp();
+db.once("open", () => {
+  runApp();
 });
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
+//Schema & Model
+const Schema = mongoose.Schema;
+
+const voteSchema = new Schema({
+  A_vote: Number,
+  B_vote: Number,
+  C_vote: Number,
+  D_vote: Number
+});
+
+const voteModel = mongoose.model("voteModel", voteSchema);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+//get current votes
+app.get("/currentVotes", (req, res) => {
+  res.send("get current Votes ");
+});
+
+//modify votes
+app.put("/modifyVotes", (req, res) => {
+  res.send("votes modified");
+});
+
+//change voting mode
+app.put("/changeMode", (req, res) => {
+  res.send("voting mode updated");
 });
 
 function runApp() {
-	const PORT = process.env.PORT || 9898;
-	app.listen(PORT, () => {
-		sysLogger.log('info', '================================================================================');
-		sysLogger.log('info', `DB Connected, listening on PORT ${PORT}`);
-	});
+  const PORT = process.env.PORT || 9898;
+  app.listen(PORT, () => {
+    sysLogger.log(
+      "info",
+      "================================================================================"
+    );
+    sysLogger.log("info", `DB Connected, listening on PORT ${PORT}`);
+  });
 }
